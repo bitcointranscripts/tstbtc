c05f17b16f618e99c576095c249d66759c597505
tvpeter <withtvpeter@gmail.com>
2023-01-02 05:07:16 +0100

    update readme and logging into to github

--- Readme.md
+++ Readme.md
@@ -14,3 +14,17 @@ To check the version:
 `python -m yttbtc -v` view the application version
 
 `python3 -m yttbtc yt2btc {video_id} {file_name}` create video transcript supplying the id of the youtube video and the source/year
+
+## OTHER REQUIREMENTS
+
+To enable us create a new repo to push the transcribed file and open a PR, install `GITHUB CLI` using the instruction [here](https://github.com/cli/cli#installation). Following the prompt, please select the below options from the prompt to login:
+
+-  what account do you want to log into? `Github.com`
+
+-  what is your preferred protocol for Git operations? `SSH`
+
+-  Upload your SSH public key to your Github account? `skip`
+
+-  How would you like to authenticate Github CLI? `Login with a web browser`
+
+copy the generated one-time pass-code and paste in the browser to authenticate
--- github.sh
+++ github.sh
@@ -5,19 +5,19 @@ read USERNAME
 
 
 # initialise the repo locally, add created file and commit
-git init
-git add "$1"
-git commit -m 'initial commit - initial transcription using yt2btc script'
-git remote add upstream https://github.com/bitcointranscripts/bitcointranscripts
+# git init -b main
+git checkout -b ${2}
+git add "$1" && git commit -m 'initial transcription using yt2btc script'
 
+gh auth login
 
-
-#  use github API to log the user in
-curl -u ${USERNAME} https://api.github.com/${USERNAME}/repos -d "{\"name\": \"${2}\"}"
+gh repo create --source=. --public
 
 # add the remote github repo to local repo and push
-git remote add origin https://github.com/${USERNAME}/${2}.git
+git remote add yttbtc git@github.com:${USERNAME}/${2}.git
 # git push --set-upstream origin master
+git push -u yttbtc main
+git remote add upstream https://github.com/bitcointranscripts/bitcointranscripts
 
-
-echo "Done. Go to https://github.com/$USERNAME/$2 to see."
+echo "Done"
+# echo "Done. Go to https://github.com/$USERNAME/$2 to see."
