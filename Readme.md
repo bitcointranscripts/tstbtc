c1ae88e5eed00d66a9fec31b1102604cfeeea281
tvpeter <withtvpeter@gmail.com>
2023-01-04 04:15:25 +0100

    replace youtube_transcript_api with youwhisper

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # YTB_TO_BTCSCRIPT
 
-This project converts youtube videos to bitcoinscripts and opens a PR on [bitcoinscript](https://github.com/bitcointranscripts/bitcointranscripts) repo. It uses `youtube_transcript_api` to transcribe the videos, then collects meta data about the video using `requests_html`. It then uses the supplied cli arguments and file to open a Pull Request on the [bitcoinscript](https://github.com/bitcointranscripts/bitcointranscripts) repo.
+This project converts YouTube videos to bitcoinscripts and opens a PR on [bitcoinscript](https://github.com/bitcointranscripts/bitcointranscripts) repo. It uses `youwhisper` to transcribe the videos, then collects meta data about the video using `requests_html`. It then uses the supplied cli arguments and file to open a Pull Request on the [bitcoinscript](https://github.com/bitcointranscripts/bitcointranscripts) repo.
 
 ## Steps:
 
@@ -8,7 +8,7 @@ The step-by-step flow for the scripts are:
 
 - transcribe given video and generate the output file
 
-- authenticate the user to github
+- authenticate the user to GitHub
 
 - fork the transcript repo/use their existing fork, clone it and branch out
 
@@ -29,18 +29,24 @@ The step-by-step flow for the scripts are:
 To check the version:
 `python -m yttbtc -v` view the application version
 
-`python3 -m yttbtc yt2btc {video_id} {file_name}` create video transcript supplying the id of the youtube video and the source/year
+`python3 -m yttbtc yt2btc {video_id} {file_name}` create video transcript supplying the id of the YouTube video and the source/year
 
 ## OTHER REQUIREMENTS
 
-To enable us fork bitcointranscript repo and open a PR, we require you to login into your github account. Kindly install `GITHUB CLI` using the instructions on their repo [here](https://github.com/cli/cli#installation). Following the prompt, please select the below options from the prompt to login:
+-  To enable us fork bitcointranscript repo and open a PR, we require you to login into your GitHub account. Kindly install `GITHUB CLI` using the instructions on their repo [here](https://github.com/cli/cli#installation). Following the prompt, please select the below options from the prompt to login:
 
--  what account do you want to log into? `Github.com`
+    -  what account do you want to log into? `Github.com`
 
--  what is your preferred protocol for Git operations? `SSH`
+    -  what is your preferred protocol for Git operations? `SSH`
 
--  Upload your SSH public key to your Github account? `skip`
+    -  Upload your SSH public key to your GitHub account? `skip`
 
--  How would you like to authenticate Github CLI? `Login with a web browser`
+    -  How would you like to authenticate GitHub CLI? `Login with a web browser`
 
-copy the generated one-time pass-code and paste in the browser to authenticate if you have enabled 2FA
+    - copy the generated one-time pass-code and paste in the browser to authenticate if you have enabled 2FA
+
+- Install `FFmpeg`
+
+     - for Mac Os users, run `brew install ffmpeg`
+
+     - for other users, follow the instruction on their [site](https://ffmpeg.org/) to install
--- yttbtc/cli.py
+++ yttbtc/cli.py
@@ -7,21 +7,84 @@ import urllib
 from typing import Optional
 import typer
 from requests_html import HTMLSession
-from youtube_transcript_api import YouTubeTranscriptApi
-import os
-
 from yttbtc import __app_name__, __version__
 import subprocess
+import pytube
+from moviepy.editor import VideoFileClip
+import pywhisper
+import os
+import static_ffmpeg
 
 app = typer.Typer()
 
 session = HTMLSession()
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
+
+def download_video(url):
+    video = pytube.YouTube(url)
+    stream = video.streams.get_by_itag(18)
+    stream.download()
+    return stream.default_filename
+
+
+def convert_to_mp3(filename):
+    clip = VideoFileClip(filename)
+    clip.audio.write_audiofile(filename[:-4] + ".mp3")
+    clip.close()
+
+
+def AudiotoText(filename):
+    model = pywhisper.load_model("base")
+    result = model.transcribe(filename)
+    print(result["text"])
+    sonuc = result["text"]
+    return sonuc
+
+
+def convert(link, model):
+    print('''
+    This tool will convert Youtube videos to mp3 files and then transcribe them to text using Whisper.
+    ''')
+    print("URL: " + link)
+    print("MODEL: " + model)
+    # FFMPEG installed on first use.
+    print("Initializing FFMPEG...")
+    static_ffmpeg.add_paths()
+
+    print("Downloading video... Please wait.")
+    try:
+        filename = download_video(link)
+        print("Downloaded video as " + filename)
+    except:
+        print("Not a valid link..")
+        return
+    try:
+        convert_to_mp3(filename)
+        print("Converted video to mp3")
+    except:
+        print("Error converting video to mp3")
+        return
+    try:
+        mymodel = pywhisper.load_model(model)
+        result = mymodel.transcribe(filename[:-4] + ".mp3")
+        print(result["text"])
+        result = result["text"]
+        os.remove(filename)
+        os.remove(filename[:-4] + ".mp3")
+        print("Removed video and audio files")
+        print("Done!")
+        return result
+    except Exception as e:
+        print("Error transcribing audio to text")
+        print(e)
+        return
 
 
 def _version_callback(value: bool) -> None:
     if value:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
+
 
 @app.callback()
 def main(
@@ -47,37 +110,28 @@ def yt2btc(
     file_name_with_ext = file_name + '.md'
     outls = []
 
-    tx = YouTubeTranscriptApi.get_transcript(video_id)
-
     url = "https://www.youtube.com/watch?v=" + video_id
-
+    result = convert(url, 'base.en')
+    
     query_string = urllib.parse.urlencode({"format": "json", "url": url})
     full_url = "https://www.youtube.com/oembed" + "?" + query_string
-
+    
     with urllib.request.urlopen(full_url) as response:
         response_text = response.read()
         data = json.loads(response_text.decode())
         title = data['title']
-
+    
     meta_data = '---\n' \
                 f'title: {title} ' + '\n' \
-                'transcript_by: youtube_transcript_api\n' \
-                f'media: {url}\n' \
-                '---\n'
+                                     f'transcript_by: youtube_to_bitcoin_transcript_v_{__version__}\n' \
+                                     f'media: {url}\n' \
+                                     '---\n'
     with open(file_name_with_ext, 'a') as opf:
         opf.write(meta_data + '\n')
-
-        for i in tx:
-            output_text = (i['text'])
-            outls.append(output_text)
-
-            opf.write(output_text + '\n')
-
-
+        opf.write(result + '\n')
+     
     absolute_path = os.path.abspath(file_name_with_ext)
 
     """ INITIALIZE AND OPEN A PR"""
     print("Initializing git and creating a repo \n")
     subprocess.call(['bash', 'github.sh', file_name_with_ext, file_name, absolute_path])
-
-
