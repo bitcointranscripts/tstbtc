adab961af8cebeecc38679e14bbc47a223a3b182
tvpeter <withtvpeter@gmail.com>
2023-01-06 04:24:30 +0100

    add model flag for users selection

--- yttbtc.py
+++ yttbtc.py
@@ -26,18 +26,23 @@ def print_version(ctx, param, value):
 @click.command()
 @click.argument('media', nargs=1)
 @click.argument('loc', nargs=1)
+@click.option('-m', '--model', type=click.Choice(['tiny', 'base', 'small', 'medium']), default = 'tiny',
+    help='Options for transcription model'
+)
 @click.option('--version',  is_flag=True, callback=print_version,
               expose_value=False, is_eager=True, help="Show the application's version and exit.")
 def add(
         media: str,
-        loc: str
+        loc: str,
+        model: str
 ) -> None:
     """Supply a YouTube video id and directory for transcription"""
     file_name = loc.replace("/", "-")
     file_name_with_ext = file_name + '.md'
 
     url = "https://www.youtube.com/watch?v=" + media
-    result = application.convert(url, 'tiny.en')
+    selected_model = model + '.en'
+    result = application.convert(url, selected_model)
 
     query_string = urllib.parse.urlencode({"format": "json", "url": url})
     full_url = "https://www.youtube.com/oembed" + "?" + query_string
