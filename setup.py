19ef2c36ad77321e692be9a00111be024c5be164
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2023-01-16 13:47:55 +0530

    fix: changed the stream type and the Readme typo

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("Readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
