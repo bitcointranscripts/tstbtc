d6cdc1ae6548e4670db3fec2d38f7038168174f8
kouloumos <kouloumosa@gmail.com>
2024-07-16 14:09:55 +0300

    move `/media` route to a different file

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 import logging
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.config import config
 from app.logging import configure_logger, get_logger
-from app.media_processor import MediaProcessor
+from routes.media import router as media_router
 
 logger = get_logger()
+configure_logger(logging.DEBUG if config.getboolean('verbose_logging', False) else logging.INFO)
 app = FastAPI()
 
 origins = [
@@ -23,22 +25,4 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-class VideoURLRequest(BaseModel):
-    youtube_url: str
-
-@app.post("/media/youtube-video-url")
-async def youtube_video_url(request: VideoURLRequest):
-    """Extract the direct video URL from a YouTube link"""
-    try:
-        configure_logger(log_level=logging.INFO)
-        processor = MediaProcessor()
-        video_url = processor.get_youtube_video_url(request.youtube_url)
-        if video_url:
-            return {"status": "success", "video_url": video_url}
-        else:
-            error_msg = f"Failed to extract the video URL for {request.youtube_url}."
-            logger.error(error_msg)
-            raise HTTPException(status_code=500, detail=error_msg)
-    except Exception as e:
-        logger.error(f"Error extracting video URL: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
+app.include_router(media_router, prefix="/media")
