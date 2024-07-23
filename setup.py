79312a16a790aabdb7b85c5a619b0b58c5c403e0
kouloumos <kouloumosa@gmail.com>
2024-07-23 12:18:49 +0300

    chore: use python 3.10

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ setup(
     py_modules=["transcriber"],
     packages=find_packages(),
     install_requires=[requirements],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     classifiers=[
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
